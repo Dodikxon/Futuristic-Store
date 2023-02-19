c489719cd808762273f8aea32f3c2d8cfcdb9403
dodikxon <ddodikxon@gmail.com>
2023-02-19 11:28:57 +0300

    add db and users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: []
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USERNAME || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'password',
+      database: process.env.POSTGRES_DATABASE || 'database',
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
-async function bootstrap() {
+async function start() {
+  const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    credentials: true,
+    origin: process.env.CLIENT_URL || 'http://localhost:3000'
+  })
+  await app.listen(PORT, () => {
+    console.log(`Server start on ${PORT}`)
+  });
 }
-bootstrap();
+start();
