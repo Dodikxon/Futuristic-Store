e5b9d54e54c5756ddc24771dc7b8e49000c4026a
dodikxon <ddodikxon@gmail.com>
2023-02-19 15:27:12 +0300

    Add authorization

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users.model';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  providers: [UsersService],
   controllers: [UsersController],
-  imports: [SequelizeModule.forFeature([User])],
+  providers: [UsersService],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    forwardRef(() => AuthModule),
+  ],
+
+  exports: [UsersService],
 })
 export class UsersModule {}
